93e609b5a27b094414d0333e881b008714ef8d2b
nicetomytyuk <igor.mytyuk@gmail.com>
2024-07-22 12:19:21 +0200

    fix: Resolve routing issues during bot imports

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env tsx
 
 import { onShutdown } from "node-graceful-shutdown";
-import { createBot } from "#root/bot/index.js";
-import { config } from "#root/config.js";
-import { createServer } from "#root/server/index.js";
+import { createBot } from "./bot/index.js";
+import { createServer } from "./server/index.js";
+import { config } from "./config.js";
 
 try {
   const bot = createBot(config.BOT_TOKEN);
